1ca249eab90dc871547cb4ea6138e6658f4698fc
petrludinin <petrludinin@gmail.com>
2026-03-27 09:56:32 +0500

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const btn = document.getElementById("themeBtn");
 
+// проверяем сохраненную тему при загрузке страницы
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+}
+
 btn.onclick = function(){
 
-document.body.classList.toggle("dark");
+    document.body.classList.toggle("dark");
+
+    // если включена темная тема
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme","dark");
+    }
+    else{
+        localStorage.setItem("theme","light");
+    }
 
 }
