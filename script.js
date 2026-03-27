8403804e1276fee11be35969f78a0eaa0d71c9ee
petrludinin <petrludinin@gmail.com>
2026-03-27 10:01:01 +0500

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const btn = document.getElementById("themeBtn");
 
-// проверяем сохраненную тему при загрузке страницы
-if(localStorage.getItem("theme") === "dark"){
+// применяем тему при загрузке страницы
+if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark");
 }
 
-btn.onclick = function(){
+if (btn) {
+    btn.addEventListener("click", function () {
 
-    document.body.classList.toggle("dark");
+        document.body.classList.toggle("dark");
 
-    // если включена темная тема
-    if(document.body.classList.contains("dark")){
-        localStorage.setItem("theme","dark");
-    }
-    else{
-        localStorage.setItem("theme","light");
-    }
+        if (document.body.classList.contains("dark")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
 
+    });
 }
